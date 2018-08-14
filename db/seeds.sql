06f7b00f902dcb6e81f67fbcda8efe92eb4dd6c1
Abdibasid Osman <mistuyeta@me.com>
2018-08-13 20:10:18 -0500

    comment

--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Fish Fillet', false, CURRENT_TIMESTAMP);
-INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Roast Beef', false, CURRENT_TIMESTAMP);
-INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Quarter Pounder', false, CURRENT_TIMESTAMP);
